2344b697db44d45cebdf868337a50e543f2d2a2f
Jonathan Wittmer <jonathan.wittmer@utexas.edu>
2019-07-17 19:07:11 -0400

    Added implicit formulation

--- spring_twist_predictor.m
+++ spring_twist_predictor.m
@@ -1,5 +1,5 @@
 % Initial code codifying equation (39) of Michalczyk
-clear, clc
+clear, clc, close all
 %% Spring geometry variables
 
 % structure definitions
@@ -12,19 +12,19 @@ gamma = gamma*pi/180;
 end_condition = possible_end_conditions{1};
 
 % number of active coils
-n_0cz = [2.5, 4.5, 6.5, 8.5, 10.5, 15.5];
+n_0cz = [4.5, 6.5, 8.5, 10.5, 15.5];
 
 % height of active coils, uncompressed, m
-H_0cz = 0.235;
+H_0cz = 0.076;
 
 % spring compression, m
-delta = 0.180;
+delta = 0.04;
 
 % Poisson's ratio
 nu = 0.3;
 
 % wire diameter, m
-D = linspace(0.01, 0.2, 1000);
+D = linspace(0.005, 0.03, 1000);
 
 %% Some derived variables
 
@@ -41,16 +41,18 @@ for j = 1:numel(n_0cz)
     R_0 = D/2;
 
     for i = 1:1000
-        theta(i,j) = compute_theta(n_0cz(j), L(i), H_0cz, H_1cz, R_0(i), nu);
+        theta(i,j) = compute_theta_implicit(n_0cz(j), L(i), H_0cz, H_1cz, R_0(i), nu);
     end
     plot(D*1000, theta(:,j)*180/pi, 'LineWidth', 3)
     hold on
     l{j} = strcat('n = ', num2str(n_0cz(j)));
 end
 
-title('Mean Diameter vs Twist') 
-xlabel('Mean Diameter, mm')
-ylabel('Twist Angle, degrees')
+title('Mean Diameter vs Twist', 'FontSize', 20, 'FontWeight', 'bold') 
+xlabel('Mean Diameter, mm', 'FontSize', 18)
+ylabel('Twist Angle, degrees', 'FontSize', 18)
 grid on
-legend(l)
+legend(l, 'FontSize', 20)
+ax = gca;
+ax.FontSize = 18
 
