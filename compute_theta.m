4d2279669d6cf5d6bb7d97433c4934fd9f45f3b8
Prajakta Bedekar <pbedekar@uh.edu>
2019-07-18 11:55:00 -0400

    Added n1_actual for consistency
    n1act is the n1 in paper.

--- compute_theta.m
+++ compute_theta.m
@@ -9,9 +9,9 @@ function [theta] = compute_theta(n0, L, H0, H1, R0, nu)
 term1 = (R0/ ((H0/(2*pi*n0))^2 + R0^2));
 term2 = H1/L * ((H0 - H1)/(2*pi*R0^2*n0* (1 + nu)));
 n1 = (L^2/sqrt(L^2 - H1^2)) * (term1 + term2);
-
+n1_act=n1/(2*pi);
 % num = L^2 * (1 + nu);
 % denom = (L^2 - H1^2)^(3/2)*rho0*(1+nu) - (H0 - H1)*H1*L*rho0;
 % n1 = ((L^2 - H1^2) / (2*pi)) * (num/denom);
 
-theta = 2*pi*(n0) - n1;
+theta = 2*pi*(n0) - 2*pi*n1_act;
