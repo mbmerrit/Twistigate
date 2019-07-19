4e70965c9776d63993fe63b8f62f305e4a283356
Jonathan Wittmer <jonathan.wittmer@utexas.edu>
2019-07-19 10:32:12 -0400

    Added nominal spring

--- compute_theta.m
+++ compute_theta.m
@@ -12,19 +12,8 @@ n_0 = Conversion_Output.n_0;
 nu = Conversion_Output.nu;
 l_w = Conversion_Output.l_w;
 
-% This is the Michalcyzk version
-% term1 = (R_0/ ((H_0/(2*pi*n_0))^2 + R_0^2));
-% term2 = H_1/l_w * ((H_0 - H_1)/(2*pi*R_0^2*n_0* (1 + nu)));
-% n_1 = (l_w^2/sqrt(l_w^2 - H_1^2)) * (term1 + term2);
-% n_1_act=n_1/(2*pi);
-
 n_1 = (l_w^2 - H_1^2) / (2*pi) * R_0 / (R_0^2 + (H_0/(2*pi*n_0))^2) * l_w^2 ...
     * (1+nu) / ((1+nu)*(l_w^2 - H_1^2)^(3/2) - (delta)*H_1*l_w);
 theta = 2*pi*(n_0 - n_1);
-
-% This section computes n1 using the new derivation - 7/18/19
-% n1_implicit = (L^2 - H1^2) / (2*pi) * R0 / (R0^2 + (H0/(2*pi*n0))^2) * L^2 * (1+nu) ...
-%       / ((1+nu)*(L^2 - H1^2)^(3/2) - (H0 - H1)*H1*L);
-% theta = 2*pi*(n0 - n1_implicit);
 
 end
---
+++ nominal_spring.m
@@ -0,0 +1,6 @@
+function spring = nominal_spring()
+	spring.d_i = 0.024;
+    spring.d_w = 0.001;
+    spring.L_free = 0.100;
+    spring.n = 7;
+end
