61e9f01a55ecb9e0b5fa941b19822fb7297f1d2a
Ellis Kessler <ellisk1@vt.edu>
2019-07-19 15:22:02 -0400

    Changed plot nu

--- Investigation/vary_nu.m
+++ Investigation/vary_nu.m
@@ -3,19 +3,19 @@ load_path
 %% Load the spring
 spring = nominal_spring;
 %% Set up the varying values for nu
-nu_vary = linspace(-.5,.5,500);
+nu_vary = linspace(0,.5,500);
 %%
-ns = 5:2:11;
+ns = 5:1:9;
 for j = 1:length(ns)
     spring.N_t = ns(j);
-    bp = Convert_Build_Params(spring);
+    spring = Convert_Build_Params(spring);
     thetas = zeros(size(nu_vary));
     for k = 1:length(nu_vary)
-        bp.nu = nu_vary(k);
-        thetas(k) = compute_theta(bp,.085);
+        spring.nu = nu_vary(k);
+        thetas(k) = compute_theta(spring,.085);
     end
     plot(nu_vary,thetas,'LineWidth',3); hold on;
-    l{j} = strcat('n_0 = ', num2str(ns(j)));
+    l{j} = strcat('n_T = ', num2str(ns(j)));
 end
 plot([.26 .33 .33 .26 .26],[10 10 53 53 10],'--k','LineWidth',2);
 l{j+1} = 'Metals';
@@ -25,4 +25,4 @@ axis tight; grid on
 xlabel("Poisson's Ratio, \nu")
 ylabel('\theta')
 ylim([0 60]);
-legend(l);
+legend(l,'location','northwest');
