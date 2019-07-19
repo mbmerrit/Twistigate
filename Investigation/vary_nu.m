6b979d44fb19c94d9f4c6f57452d03b6cdde8528
Ellis Kessler <ellisk1@vt.edu>
2019-07-19 12:03:28 -0400

    Update nu plotting script

--- Investigation/vary_nu.m
+++ Investigation/vary_nu.m
@@ -1,10 +1,11 @@
 clear all; close all; clc;
+load_path
 %% Load the spring
 spring = nominal_spring;
 %% Set up the varying values for nu
-nu_vary = linspace(.2,.4,100);
+nu_vary = linspace(-.5,.5,500);
 %%
-ns = 3:2:11;
+ns = 5:2:11;
 for j = 1:length(ns)
     spring.N_t = ns(j);
     bp = Convert_Build_Params(spring);
@@ -16,10 +17,12 @@ for j = 1:length(ns)
     plot(nu_vary,thetas,'LineWidth',3); hold on;
     l{j} = strcat('n_0 = ', num2str(ns(j)));
 end
+plot([.26 .33 .33 .26 .26],[10 10 53 53 10],'--k','LineWidth',2);
+l{j+1} = 'Metals';
 %%
 set(gca,'fontsize', 20)
 axis tight; grid on
-xlabel('\nu')
+xlabel("Poisson's Ratio, \nu")
 ylabel('\theta')
-ylim([0 100]);
+ylim([0 60]);
 legend(l);
